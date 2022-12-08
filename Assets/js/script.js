0d8bd0f12c17741e4793092ade352c2e224a1fb8
saramangelo <saramangelo@gmail.com>
2022-12-07 23:15:19 -0800

    cleaned up code, deleted extra comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -20,19 +20,7 @@
 // best practice - keep variables where you need them as opposed to all being globally scoped
 
 // jQuery
-var pastEl = $(".past"); // 9 AM
-var presentEl = $(".present"); // 10 AM
-var futureEl = $(".future"); // 11 AM
 var timeBlockEl = $(".time-block");
-var hourNine = $("#hour-9");
-var hourTen = $("#hour-10");
-var hourEleven = $("#hour-11");
-var hourTwelve = $("#hour-12");
-var hourThirteen = $("#hour-13");
-var hourFourteen = $("#hour-14");
-var hourFifteen = $("#hour-15");
-var hourSixteen = $("#hour-16");
-var hourSeventeen = $("#hour-17");
 var today = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
 var currentHour = dayjs().hour();
 var hourTimeNumber = [];
@@ -44,26 +32,6 @@ for (var i = 0; i < timeBlockEl.length; i++) {
     hourTimeNumber.push(parseInt(elementID[5] + elementID[6]));
 }
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage? 3rd √
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time? 2nd √
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this? 4th
-  //
-  // TODO: Add code to display the current date in the header of the page. 1st √
-});
 
 // FUNCTION TO UPDATE COLOR BLOCK--SHOULD THIS BE RUNNING WITH TIMER/CLOCK? -- HOW DO I KNOW IF THIS IS WORKING?
 
@@ -81,16 +49,13 @@ function checkTime() {
 // FUNCTION TO GET FROM LOCAL STORAGE AND DISPLAY USER INPUT ON TEXTAREA
 
 function renderUserInput() {
-// timeBlockEl textarea - child element
 
-// post data into textarea
-for (var i = 0; i < timeBlockEl.length; i++) {
-  var key = timeBlockEl[i].getAttribute("id");
-  var value = localStorage.getItem(key);
-timeBlockEl[i].children[1].innerText = value;
+  for (var i = 0; i < timeBlockEl.length; i++) {
+    var key = timeBlockEl[i].getAttribute("id");
+    var value = localStorage.getItem(key);
+    timeBlockEl[i].children[1].innerText = value;
+  }
 }
-}
-//
 
 // FUNCTION TO SAVE USER INPUT ON TEXTAREA
 
